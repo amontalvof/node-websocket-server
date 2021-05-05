4688ec02797e590abcd26a0fcb186d29eb28a0be
amontalvof <amontalvof92@gmail.com>
2021-05-05 16:32:42 -0500

    feat: socket.io installation

--- app.js
+++ app.js
@@ -4,8 +4,9 @@ const cors = require('cors');
 const colors = require('colors/safe');
 
 const app = express();
+const server = require('http').createServer(app);
+const io = require('socket.io')(server);
 const port = process.env.PORT;
-const paths = {};
 
 const middlewares = () => {
     // CORS
@@ -14,19 +15,13 @@ const middlewares = () => {
     app.use(express.static('public'));
 };
 
-const routes = () => {
-    // routes here
-};
-
 const listen = () => {
-    app.listen(port, () => {
+    server.listen(port, () => {
         console.log(colors.cyan('Server running on port', port));
     });
 };
 
 // middlewares
 middlewares();
-//routes of my app
-routes();
 
 listen();
