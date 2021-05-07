b1da8913c8da22b82dd714ba866847dc1d1bbb83
amontalvof <amontalvof92@gmail.com>
2021-05-07 11:19:01 -0500

    feat: connection and disconnection messages

--- public/js/socket-client.js
+++ public/js/socket-client.js
@@ -1,1 +1,16 @@
-const socket = io()
+// HTML References
+const lblOnline = document.querySelector('#lblOnline');
+const lblOffline = document.querySelector('#lblOffline');
+
+const socket = io();
+
+socket.on('connect', () => {
+    console.log('Connected to the server');
+    lblOffline.style.display = 'none';
+    lblOnline.style.display = '';
+});
+socket.on('disconnect', () => {
+    console.log('Disconnected from server');
+    lblOnline.style.display = 'none';
+    lblOffline.style.display = '';
+});
